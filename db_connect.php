abc2c1a42fd7ec0a6353cebb26c68d0b2893593c
Naeem Shj <naeem.shjint@gmail.com>
2025-03-07 21:39:15 +0500

    a

--- db_connect.php
+++
@@ -1,3 +0,0 @@
-<?php 
-date_default_timezone_set("Asia/Karachi");
-$conn= new mysqli('localhost','vdoitcom_task','taskmanagement','vdoitcom_taskmanagement')or die("Could not connect to mysql".mysqli_error($conn));
